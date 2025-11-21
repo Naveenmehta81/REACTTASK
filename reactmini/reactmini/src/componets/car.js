a083137eab54e6f7c36c22785b99539a48a318d7
Naveenmehta81 <naveenmehta2002@gmail.com>
2025-11-21 10:28:36 +0530

    react jsx and props
    understanding jsx code and props

---
+++ reactmini/reactmini/src/componets/car.js
@@ -0,0 +1,8 @@
+
+function Car(props){
+    return(
+       <h1>hy i make a component used in props {props.brand} and  year is {props.year[0]} , {props.year[1]}</h1>
+    );
+}
+
+export default Car; 
---
+++ reactmini/reactmini/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Car from './componets/car.js';
+
+
+// const myelement = (
+//   <div>
+//     <p>i need to command in jsx.</p>
+//   </div>
+// );
+
+// function Car(){
+//   return(
+//     <div>
+//        <h1>my car </h1>
+//     <p>i used a car function</p>
+//     </div>
+    
+//   );
+// };
+
+
+// function Kwtohp(kw){
+//   return kw*2
+// };
+
+// function Car(){
+//   return(
+//     <div>
+//        <h1>my car </h1>
+//     <p>my car has{Kwtohp(2)} are </p>
+//     </div>
+    
+//   );
+// };
+
+// function Car(props){
+//   return(
+//     <div>
+//       <p>my car clour{props.color}</p>
+//     </div>
+//   )
+// }
+
+let x = "ford";
+let y = [1192 ,323, 333];
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  //  <Car/>
+      
+    //  <Car color = "red" />
+
+     <Car brand={x} year ={y}/>
+     
+);
+
+
+reportWebVitals();
